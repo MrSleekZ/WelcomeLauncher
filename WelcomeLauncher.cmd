85de2f1f40e2eae1224da7a5f103eba401857e66
MrSleekZ <40346672+MrSleekZ@users.noreply.github.com>
2019-03-02 22:31:12 +1100

    Add files via upload

---
+++ WelcomeLauncher.cmd
@@ -0,0 +1,23 @@
+@echo off
+color 0a
+title WelcomeLauncher
+
+echo Welcome back, *Your name here*!
+
+echo -----------------------
+
+echo WelcomeLauncher made by MrSleekZ (22/02/2019)
+
+echo Version 1.3 (26/02/2019)
+
+echo -----------------------
+
+echo * Change log:
+
+echo Fixed code
+
+echo Added a UI
+
+echo Fixed Date
+
+pause
